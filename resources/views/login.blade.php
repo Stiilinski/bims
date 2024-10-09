0ba269ac31dceb06d6ddecdfe71d38eb47c0142d
Stilinski <realesdarren@gmail.com>
2024-10-09 23:32:17 +0800

    edit changes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,32 +1,132 @@
-@extends('layouts.loginLayout')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('styles')
-    <link rel="stylesheet" href="/css/login.css">
-@endsection
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-@section('content')
-    <div class="loginContainer">        
-        <form class="ms-auto me-auto mt-3 loginForm" action="{{ route('regValidation.check') }}" method="POST">
-            @csrf
-            <img src="/images/logo.png" alt="Barangay Logo" class="brgyLogo">
-            
-            @if ($errors->has('fail'))
-              <div class="alert alert-danger">
-                  {{ $errors->first('fail') }}
+  <title>Login - Barangay Information Management System</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Updated: Apr 20 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <img src="assets/img/logo.png" alt="">
+                  <span class="d-none d-lg-block">B.I.M.S</span>
+                </a>
+              </div><!-- End Logo -->
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                    <p class="text-center small">Enter your Employee ID & Password to login</p>
+                  </div>
+
+                  @if ($errors->has('fail'))
+                  <div class="alert alert-danger">
+                      {{ $errors->first('fail') }}
+                  </div>
+                @endif
+
+                  <form class="row g-3 needs-validation" action="{{ route('regValidation.check') }}" method="POST">
+                    @csrf
+                    <div class="col-12">
+                      <label for="employeeId" class="form-label">Employee ID</label>
+                        <input type="text" name="employeeId" class="form-control" id="employeeId" placeholder="Enter Employee ID" required>
+                        <span class="text-danger">@error('employeeId') {{ $message }} @enderror</span>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="passwords" class="form-label">Password</label>
+                      <input type="password" name="passwords" class="form-control" id="passwords" placeholder="Enter Password" required>
+                      <span class="text-danger">@error('passwords') {{ $message }} @enderror</span>
+                    </div>
+
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Login</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0">Don't have account? <a href="{{Route('register')}}">Create an account</a></p>
+                    </div>
+                  </form>
+
+                </div>
               </div>
-            @endif
 
-            <div class="mb-3">
-              <label for="employeeId" class="form-label">Employee ID</label>
-              <input type="text" class="form-control" name="employeeId" id="employeeId" placeholder="Enter Employee ID">
-              <span class="text-danger">@error('employeeId') {{ $message }} @enderror</span>
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+                {{-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> --}}
+              </div>
+
             </div>
-            <div class="mb-3">
-              <label for="passwords" class="form-label">Password</label>
-              <input type="password" class="form-control" name="passwords" id="passwords" placeholder="Enter Password">
-              <span class="text-danger">@error('passwords') {{ $message }} @enderror</span>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-          </form>
+          </div>
+        </div>
+
+      </section>
+
     </div>
-@endsection
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,76 +1,147 @@
-@extends('layouts.loginLayout')
-@section('styles')
-    <link rel="stylesheet" href="/css/registration.css">
-@endsection
-@section('content')
-    <div class="ms-auto me-auto regContainer">
-        <h2 class="registerTitle">Employee Registration</h2>        
-            <form class="ms-auto me-auto mt-3 regForm" method="POST" action="{{ route('regValidation.save')}}" id="mainForm" autocomplete="off" enctype="multipart/form-data">
-                @csrf
-                    <div class="personalInfo">
-                        <div class="mb-3">
-                            <label for="fname" class="form-label">First Name</label>
-                            <input type="text" class="form-control" id="fname" name="fname" placeholder="Enter First Name">
-                            <span class="text-danger error-text fname_error"></span>
+<!DOCTYPE html>
+<html lang="en">
 
-                        </div>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-                        <div class="mb-3">
-                            <label for="lname" class="form-label">Last Name</label>
-                            <input type="text" class="form-control" id="lname" name="lname" placeholder="Enter Last Name">
-                            <span class="text-danger error-text lname_error"></span>
-                        </div>
+  <title>Pages / Register - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-                        <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="text" class="form-control" id="email" name="email" placeholder="Enter Email" autocomplete="off">
-                            <span class="text-danger error-text email_error"></span>
-                        </div>
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-                        <div class="mb-3">
-                            <label for="address" class="form-label">Address</label>
-                            <input type="text" class="form-control" id="address" name="address" placeholder="Enter Address" autocomplete="off">
-                            <span class="text-danger error-text address_error"></span>
-                        </div>
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
-                        <div class="mb-3">
-                            <label for="profile" class="form-label">Profile Picture</label>
-                            <input type="file" class="form-control" id="profile" name="profile" aria-describedby="inputGroupFileAddon03" aria-label="Upload">
-                            <span class="text-danger error-text profile_error"></span>
-                        </div>
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Updated: Apr 20 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <img src="assets/img/logo.png" alt="">
+                  <span class="d-none d-lg-block">B.I.M.S</span>
+                </a>
+              </div><!-- End Logo -->
+
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Create an Account</h5>
+                    <p class="text-center small">Enter your personal details to create account</p>
+                  </div>
+
+                  <form class="row g-3 needs-validation" novalidate>
+                    <div class="col-12">
+                      <label for="yourName" class="form-label">Your Name</label>
+                      <input type="text" name="name" class="form-control" id="yourName" required>
+                      <div class="invalid-feedback">Please, enter your name!</div>
                     </div>
 
-                    <div class="accountInfo">
-                        <div class="mb-3">
-                            <label for="position" class="form-label">Employee Work Position</label>
-                            <input type="text" class="form-control" id="position" name="position" placeholder="Enter Work Position">
-                            <span class="text-danger error-text position_error"></span>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="contact" class="form-label">Employee Contact</label>
-                            <input type="text" class="form-control" id="contact" name="contact" placeholder="Enter Contact">
-                            <span class="text-danger error-text contact_error"></span>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="employeeId" class="form-label">Employee ID</label>
-                            <input type="text" class="form-control" id="employeeId" name="employeeId" placeholder="Enter Employee ID">
-                            <span class="text-danger error-text employeeId_error"></span>
-                        </div>
-
-                        <div class="mb-3">
-                            <label for="passwords" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="passwords" name="passwords" placeholder="Enter Password">
-                            <span class="text-danger error-text passwords_error"></span>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="col-12">
+                      <label for="yourEmail" class="form-label">Your Email</label>
+                      <input type="email" name="email" class="form-control" id="yourEmail" required>
+                      <div class="invalid-feedback">Please enter a valid Email adddress!</div>
                     </div>
-                
-            </form>
+
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Username</label>
+                      <div class="input-group has-validation">
+                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                        <input type="text" name="username" class="form-control" id="yourUsername" required>
+                        <div class="invalid-feedback">Please choose a username.</div>
+                      </div>
+                    </div>
+
+                    <div class="col-12">
+                      <label for="yourPassword" class="form-label">Password</label>
+                      <input type="password" name="password" class="form-control" id="yourPassword" required>
+                      <div class="invalid-feedback">Please enter your password!</div>
+                    </div>
+
+                    <div class="col-12">
+                      <div class="form-check">
+                        <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms" required>
+                        <label class="form-check-label" for="acceptTerms">I agree and accept the <a href="#">terms and conditions</a></label>
+                        <div class="invalid-feedback">You must agree before submitting.</div>
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Create Account</button>
+                    </div>
+                    <div class="col-12">
+                      <p class="small mb-0">Already have an account? <a href="pages-login.html">Log in</a></p>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+                {{-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> --}}
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
     </div>
-    <script src="/js/jquery-3.5.0.min.js"></script>
-    <script src="/js/main.js"></script>
-@endsection
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
